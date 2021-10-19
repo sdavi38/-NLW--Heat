85cbb9ae37b1b66441a0d2e123c2e60815b69865
David Bernardo <sdavi28@hotmail.com>
2021-10-19 09:31:05 -0300

    config rota

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
+import  "dotenv/config"
 import express from 'express'
 const PORT = 3000
 
 const app = express()
+
+//ROTA DE AUTENTICAÇÃO NO GITHUB
+
+app.get("/github", (request, response)=>{
+    response.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${process.env.
+    GITHUB_CLIENT_ID}`
+    )
+})
 
 app.listen(PORT , () => console.log( `Server is running on ${PORT}`))
